f213d2a8fe1f918cebf206872a23f56b4faa8b4e
Eric Stokes <fernferret@gmail.com>
2011-06-19 16:42:39 -0600

    Add "real code" kinda

---
+++ src/com/onarandombox/MultiverseNetherPortals/MultiverseNetherPortals.java
@@ -0,0 +1,16 @@
+package com.onarandombox.MultiverseNetherPortals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MultiverseNetherPortals extends JavaPlugin{
+    @Override
+    public void onEnable() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void onDisable() {
+        // TODO Auto-generated method stub
+        
+    }
+}
