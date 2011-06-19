838949438a62674da45b2bbcda7a51e4aace605b
Eric Stokes <fernferret@gmail.com>
2011-06-19 17:33:24 -0600

    Base template for a Multiverse-Core Plugin is complete

---
+++ src/com/onarandombox/MultiverseNetherPortals/MVNPPluginListener.java
@@ -0,0 +1,22 @@
+package com.onarandombox.MultiverseNetherPortals;
+
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.event.server.ServerListener;
+
+import com.onarandombox.MultiverseCore.MultiverseCore;
+
+public class MVNPPluginListener extends ServerListener {
+	
+	MultiverseNetherPortals plugin;
+	
+	public MVNPPluginListener(MultiverseNetherPortals plugin) {
+		this.plugin = plugin;
+	}
+	
+	public void onPluginEnable(PluginEnableEvent event) {
+		if (event.getPlugin().getDescription().getName().equals("Multiverse-Core")) {
+			this.plugin.core = ((MultiverseCore) plugin.getServer().getPluginManager().getPlugin("Multiverse-Core"));
+			this.plugin.getServer().getPluginManager().enablePlugin(this.plugin);
+		}
+	}
+}
--- src/com/onarandombox/MultiverseNetherPortals/MultiverseNetherPortals.java
+++ src/com/onarandombox/MultiverseNetherPortals/MultiverseNetherPortals.java
@@ -1,16 +1,58 @@
 package com.onarandombox.MultiverseNetherPortals;
 
+import java.io.File;
+import java.util.logging.Logger;
+
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event.Type;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MultiverseNetherPortals extends JavaPlugin{
-    @Override
-    public void onEnable() {
-        // TODO Auto-generated method stub
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import com.onarandombox.utils.DebugLog;
+
+public class MultiverseNetherPortals extends JavaPlugin {
+	
+	public static final Logger log = Logger.getLogger("Minecraft");
+	public static final String logPrefix = "[MultiVerse-NetherPortals] ";
+	protected static DebugLog debugLog;
+	protected MultiverseCore core;
+	protected MVNPPluginListener pluginListener;
+	
+	
+	@Override
+	public void onEnable() {
+		pluginListener = new MVNPPluginListener(this);
+	    // Register the PLUGIN_ENABLE Event as we will need to keep an eye out for the Core Enabling if we don't find it initially.
+        getServer().getPluginManager().registerEvent(Type.PLUGIN_ENABLE, pluginListener, Priority.Normal, this);
         
-    }
-    @Override
-    public void onDisable() {
-        // TODO Auto-generated method stub
-        
-    }
+		log.info(logPrefix + "- Version " + this.getDescription().getVersion() + " Enabled - By " + getAuthors());
+	}
+	
+	@Override
+	public void onDisable() {
+		log.info(logPrefix + "- Disabled");
+	}
+	
+	@Override
+	public void onLoad() {
+		getDataFolder().mkdirs();
+		debugLog = new DebugLog("Multiverse-NetherPortals", getDataFolder() + File.separator + "debug.log");
+	}
+	
+	/**
+	 * Parse the Authors Array into a readable String with ',' and 'and'.
+	 * 
+	 * @return
+	 */
+	private String getAuthors() {
+		String authors = "";
+		for (int i = 0; i < this.getDescription().getAuthors().size(); i++) {
+			if (i == this.getDescription().getAuthors().size() - 1) {
+				authors += " and " + this.getDescription().getAuthors().get(i);
+			} else {
+				authors += ", " + this.getDescription().getAuthors().get(i);
+			}
+		}
+		return authors.substring(2);
+	}
 }
