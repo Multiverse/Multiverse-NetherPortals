38c63c18770b09d3784df0913398c5caa32358f9
Waester <waestergamer@gmail.com>
2014-02-28 11:03:53 +0100

    Update MVNPEntityListener.java

--- src/main/java/com/onarandombox/MultiverseNetherPortals/listeners/MVNPEntityListener.java
+++ src/main/java/com/onarandombox/MultiverseNetherPortals/listeners/MVNPEntityListener.java
@@ -2,25 +2,19 @@ package com.onarandombox.MultiverseNetherPortals.listeners;
 
 import com.onarandombox.MultiverseCore.api.LocationManipulation;
 import com.onarandombox.MultiverseCore.api.MVWorldManager;
-import com.onarandombox.MultiverseCore.api.MultiverseMessaging;
 import com.onarandombox.MultiverseCore.api.MultiverseWorld;
 import com.onarandombox.MultiverseCore.event.MVPlayerTouchedPortalEvent;
 import com.onarandombox.MultiverseCore.utils.PermissionTools;
 import com.onarandombox.MultiverseNetherPortals.MultiverseNetherPortals;
 import com.onarandombox.MultiverseNetherPortals.enums.PortalType;
 import com.onarandombox.MultiverseNetherPortals.utils.MVLinkChecker;
-import com.onarandombox.MultiverseNetherPortals.utils.MVNameChecker;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPortalEnterEvent;
-import org.bukkit.util.Vector;
-
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,12 +23,10 @@ import java.util.logging.Level;
 public class MVNPEntityListener implements Listener {
 
     private MultiverseNetherPortals plugin;
-    private MVNameChecker nameChecker;
     private MVLinkChecker linkChecker;
     private MVWorldManager worldManager;
     private PermissionTools pt;
     private int cooldown = 250;
-    private MultiverseMessaging messaging;
     private Map<String, Date> playerErrors;
     private Map<String, Location> eventRecord;
     private LocationManipulation locationManipulation;
@@ -44,46 +36,13 @@ public class MVNPEntityListener implements Listener {
 
     public MVNPEntityListener(MultiverseNetherPortals plugin) {
         this.plugin = plugin;
-        this.nameChecker = new MVNameChecker(this.plugin);
         this.linkChecker = new MVLinkChecker(this.plugin);
         this.worldManager = this.plugin.getCore().getMVWorldManager();
         this.pt = new PermissionTools(this.plugin.getCore());
         this.playerErrors = new HashMap<String, Date>();
         this.eventRecord = new HashMap<String, Location>();
-        this.messaging = this.plugin.getCore().getMessaging();
         this.locationManipulation = this.plugin.getCore().getLocationManipulation();
 
-    }
-
-    protected void shootPlayer(Player p, Block block, PortalType type) {
-        if (!plugin.isUsingBounceBack()) {
-            this.plugin.log(Level.FINEST, "You said not to use bounce back so the player is free to walk into portal!");
-            return;
-        }
-        this.playerErrors.put(p.getName(), new Date());
-        double myconst = 2;
-        double newVecX = 0;
-        double newVecZ = 0;
-        // Determine portal axis:
-        BlockFace face = p.getLocation().getBlock().getFace(block);
-        if (block.getRelative(BlockFace.EAST).getType() == Material.PORTAL || block.getRelative(BlockFace.WEST).getType() == Material.PORTAL) {
-            this.plugin.log(Level.FINER, "Found Portal: East/West");
-            if (p.getLocation().getX() < block.getLocation().getX()) {
-                newVecX = -1 * myconst;
-            } else {
-                newVecX = 1 * myconst;
-            }
-        } else {
-            //NOrth/South
-            this.plugin.log(Level.FINER, "Found Portal: North/South");
-            if (p.getLocation().getZ() < block.getLocation().getZ()) {
-                newVecZ = -1 * myconst;
-            } else {
-                newVecZ = 1 * myconst;
-            }
-        }
-        p.teleport(p.getLocation().clone().add(newVecX, .2, newVecZ));
-        p.setVelocity(new Vector(newVecX, .6, newVecZ));
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
@@ -114,12 +73,6 @@ public class MVNPEntityListener implements Listener {
         }
         MVPlayerTouchedPortalEvent playerTouchedPortalEvent = new MVPlayerTouchedPortalEvent(p, event.getLocation());
         this.plugin.getServer().getPluginManager().callEvent(playerTouchedPortalEvent);
-        Location eventLocation = event.getLocation().clone();
-        if (!playerTouchedPortalEvent.canUseThisPortal()) {
-            // Someone else said the player is not allowed to go here.
-            this.shootPlayer(p, eventLocation.getBlock(), PortalType.NETHER);
-            this.plugin.log(Level.FINEST, "Someone request this player be kicked back!!");
-        }
         if(playerTouchedPortalEvent.isCancelled()) {
             this.plugin.log(Level.FINEST, "Someone cancelled the enter Event for NetherPortals!");
             return;
@@ -138,7 +91,6 @@ public class MVNPEntityListener implements Listener {
             type = PortalType.NETHER;
         }
 
-        String currentWorld = event.getLocation().getWorld().getName();
         String linkedWorld = this.plugin.getWorldLink(event.getLocation().getWorld().getName(), type);
         Location currentLocation = event.getLocation();
 
@@ -146,53 +98,21 @@ public class MVNPEntityListener implements Listener {
 
         if (linkedWorld != null) {
             toLocation = this.linkChecker.findNewTeleportLocation(currentLocation, linkedWorld, p);
-        } else if (this.nameChecker.isValidNetherName(currentWorld)) {
-            if (type == PortalType.NETHER) {
-                toLocation = this.linkChecker.findNewTeleportLocation(currentLocation, this.nameChecker.getNormalName(currentWorld, PortalType.NETHER), p);
-            } else {
-                toLocation = this.linkChecker.findNewTeleportLocation(currentLocation, this.nameChecker.getEndName(this.nameChecker.getNormalName(currentWorld, PortalType.NETHER)), p);
-            }
-        } else if (this.nameChecker.isValidEndName(currentWorld)) {
-            if (type == PortalType.NETHER) {
-                toLocation = this.linkChecker.findNewTeleportLocation(currentLocation, this.nameChecker.getNetherName(this.nameChecker.getNormalName(currentWorld, PortalType.END)), p);
-            } else {
-                toLocation = this.linkChecker.findNewTeleportLocation(currentLocation, this.nameChecker.getNormalName(currentWorld, PortalType.END), p);
-            }
-        } else {
-            if(type == PortalType.END) {
-                toLocation = this.linkChecker.findNewTeleportLocation(currentLocation, this.nameChecker.getEndName(currentWorld), p);
-            } else {
-                toLocation = this.linkChecker.findNewTeleportLocation(currentLocation, this.nameChecker.getNetherName(currentWorld), p);
-            }
         }
 
         if (toLocation == null) {
-            this.shootPlayer(p, eventLocation.getBlock(), type);
-            this.messaging.sendMessage(p, "This portal goes nowhere!", false);
-            if (type == PortalType.END) {
-                this.messaging.sendMessage(p, "No specific end world has been linked to this world and '" + this.nameChecker.getEndName(currentWorld) + "' is not a world.", true);
-            } else {
-                this.messaging.sendMessage(p, "No specific nether world has been linked to this world and '" + this.nameChecker.getNetherName(currentWorld) + "' is not a world.", true);
-            }
             return;
         }
         MultiverseWorld fromWorld = this.worldManager.getMVWorld(p.getLocation().getWorld().getName());
         MultiverseWorld toWorld = this.worldManager.getMVWorld(toLocation.getWorld().getName());
-        if (fromWorld.getCBWorld().equals(toWorld.getCBWorld())) {
-            // The player is Portaling to the same world.
-            this.plugin.log(Level.FINER, "Player '" + p.getName() + "' is portaling to the same world.");
-            return;
-        }
         if (!pt.playerHasMoneyToEnter(fromWorld, toWorld, p, p, false)) {
             System.out.println("BOOM");
-            this.shootPlayer(p, eventLocation.getBlock(), type);
             this.plugin.log(Level.FINE, "Player '" + p.getName() + "' was DENIED ACCESS to '" + toWorld.getCBWorld().getName() +
                     "' because they don't have the FUNDS required to enter.");
             return;
         }
         if (this.plugin.getCore().getMVConfig().getEnforceAccess()) {
             if (!pt.playerCanGoFromTo(fromWorld, toWorld, p, p)) {
-                this.shootPlayer(p, eventLocation.getBlock(), type);
                 this.plugin.log(Level.FINE, "Player '" + p.getName() + "' was DENIED ACCESS to '" + toWorld.getCBWorld().getName() +
                         "' because they don't have: multiverse.access." + toWorld.getCBWorld().getName());
             }
