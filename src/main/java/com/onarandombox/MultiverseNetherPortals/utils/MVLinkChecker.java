fe18c4be08998a2649683c9491723d996be6d7d4
Waester <waestergamer@gmail.com>
2014-03-20 09:32:00 +0100

    Fix mobs not passing through portals correctly. Fixes #72

--- src/main/java/com/onarandombox/MultiverseNetherPortals/utils/MVLinkChecker.java
+++ src/main/java/com/onarandombox/MultiverseNetherPortals/utils/MVLinkChecker.java
@@ -3,8 +3,9 @@ package com.onarandombox.MultiverseNetherPortals.utils;
 import com.onarandombox.MultiverseCore.api.MVWorldManager;
 import com.onarandombox.MultiverseCore.api.MultiverseWorld;
 import com.onarandombox.MultiverseNetherPortals.MultiverseNetherPortals;
+
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityPortalEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 
 import java.util.logging.Level;
@@ -18,27 +19,18 @@ public class MVLinkChecker {
         this.worldManager = this.plugin.getCore().getMVWorldManager();
     }
 
-    public Location findNewTeleportLocation(Location fromLocation, String worldstring, Player p) {
+    public void findNewTeleportLocation(EntityPortalEvent event, Location fromLocation, String worldstring) {
         MultiverseWorld tpto = this.worldManager.getMVWorld(worldstring);
 
-        if (tpto == null) {
-            this.plugin.log(Level.FINE, "Can't find world " + worldstring);
-        } else if (!this.plugin.getCore().getMVPerms().canEnterWorld(p, tpto)) {
-            this.plugin.log(Level.WARNING, "Player " + p.getName() + " can't enter world " + worldstring);
-        } else if (!this.worldManager.isMVWorld(fromLocation.getWorld().getName())) {
-            this.plugin.log(Level.WARNING, "World " + fromLocation.getWorld().getName() + " is not a Multiverse world");
-        } else {
-            this.plugin.log(Level.FINE, "Finding new teleport location for player " + p.getName() + " to world " + worldstring);
-
+        if (tpto != null) {
             // Set the output location to the same XYZ coords but different world
-            double toScaling = this.worldManager.getMVWorld(tpto.getName()).getScaling();
-            double fromScaling = this.worldManager.getMVWorld(fromLocation.getWorld().getName()).getScaling();
+            double toScaling = tpto.getScaling();
+            double fromScaling = this.worldManager.getMVWorld(event.getFrom().getWorld().getName()).getScaling();
 
             fromLocation = this.getScaledLocation(fromLocation, fromScaling, toScaling);
             fromLocation.setWorld(tpto.getCBWorld());
-            return fromLocation;
         }
-        return null;
+        event.setTo(fromLocation);
     }
 
     public void getNewTeleportLocation(PlayerPortalEvent event, Location fromLocation, String worldstring) {
@@ -47,7 +39,9 @@ public class MVLinkChecker {
         if (tpto == null) {
             this.plugin.log(Level.FINE, "Can't find " + worldstring);
         } else if (!this.plugin.getCore().getMVPerms().canEnterWorld(event.getPlayer(), tpto)) {
-            this.plugin.log(Level.WARNING, "Player " + event.getPlayer().getName() + " can't enter world " + worldstring);
+            if (plugin.isUsingBounceBack()) {
+                this.plugin.log(Level.WARNING, "Player " + event.getPlayer().getName() + " can't enter world " + worldstring);
+            }
         } else if (!this.worldManager.isMVWorld(fromLocation.getWorld().getName())) {
             this.plugin.log(Level.WARNING, "World " + fromLocation.getWorld().getName() + " is not a Multiverse world");
         } else {
